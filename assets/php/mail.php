4098ff21c18c1460110fcfa1c1e2df40b68b9460
EARA-dean <dean@earadesigns.co.za>
2014-04-30 11:31:24 +0200

    navbar update

---
+++ assets/php/mail.php
@@ -0,0 +1,25 @@
+<?php
+    $to = 'your@domain.com';
+    $from = 'your@domain.com';
+    $subject = 'Subject';
+
+    $message = '';
+    $message .= '<table cellpadding="0" cellspacing="0">';
+
+    foreach ($_POST['form_data'] as $postname => $post) {
+
+        if ($postname != "recaptcha_challenge_field" && $postname != "recaptcha_response_field") {
+
+            $message .= "<tr><td style='padding: 5px 20px 5px 5px'><strong>" . urldecode($postname) . ":</strong>" . "</td><td style='padding: 5px; color: #444'>" . $post . "</td></tr>";
+        }
+    }
+
+    $message .= '</table>';
+
+    $headers = 'From: ' . $from . "\r\n" .
+            'Reply-To: ' . $from . "\r\n" .
+            'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+?>
---
+++ non-used code snippets/Snippet Collections.php
@@ -0,0 +1,45 @@
+<!-- Showcase section on altus website-->
+<div class="row no-margin">
+				<ul class="posts">
+					<li>
+						<img src="images/demo/posts-1.jpg" alt="Amazing showcase 1">
+						<div>
+							<h2>Amazing showcase of your items</h2>
+							<p>Lorem ipsum dolor sit amet, co sectetur adiping elit. Etiam placerat odio nec quam aliquet.</p>
+							<a href="portfolio-single.html" class="fa fa-link"></a>
+						</div>
+					</li>
+					<li>
+						<img src="images/demo/posts-2.jpg" alt="Amazing showcase 2">
+						<div>
+							<h2>Amazing showcase of your items</h2>
+							<p>Lorem ipsum dolor sit amet, co sectetur adiping elit. Etiam placerat odio nec quam aliquet.</p>
+							<a href="portfolio-single.html" class="fa fa-link"></a>
+						</div>
+					</li>
+					<li>
+						<img src="images/demo/posts-3.jpg" alt="Amazing showcase 3">
+						<div>
+							<h2>Amazing showcase of your items</h2>
+							<p>Lorem ipsum dolor sit amet, co sectetur adiping elit. Etiam placerat odio nec quam aliquet.</p>
+							<a href="portfolio-single.html" class="fa fa-link"></a>
+						</div>
+					</li>
+					<li>
+						<img src="images/demo/posts-4.jpg" alt="Amazing showcase 4">
+						<div>
+							<h2>Amazing showcase of your items</h2>
+							<p>Lorem ipsum dolor sit amet, co sectetur adiping elit. Etiam placerat odio nec quam aliquet.</p>
+							<a href="portfolio-single.html" class="fa fa-link"></a>
+						</div>
+					</li>
+					<li>
+						<img src="images/demo/posts-5.jpg" alt="Amazing showcase 5">
+						<div>
+							<h2>Amazing showcase of your items</h2>
+							<p>Lorem ipsum dolor sit amet, co sectetur adiping elit. Etiam placerat odio nec quam aliquet.</p>
+							<a href="portfolio-single.html" class="fa fa-link"></a>
+						</div>
+					</li>
+				</ul>
+			</div>
